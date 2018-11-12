f36d8f0c6629c64761b558ac41465ca4f4e46b4c
David Mayou <davidpmayou@gmail.com>
2018-11-12 13:16:14 -0600

    add div with counter on click

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+'use strict';
+let numClicks = 0;
+
+$(document).ready(onReady);
+
+function onReady() {
+    $('#generateBtn').on('click', generateClicked);
+}
+
+function generateClicked() {
+    numClicks++;
+    $('body')
+        .append('<div></div>')
+        .append(`<p>${numClicks}</p>`);
+}
