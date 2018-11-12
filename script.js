2f8b2f307de1245731112f79b3623e4cf61526d4
David Mayou <davidpmayou@gmail.com>
2018-11-12 13:37:26 -0600

    add div color swap

--- script.js
+++ script.js
@@ -8,23 +8,28 @@ function onReady() {
 }
 
 function generateClicked() {
-    numClicks++;
-    $('<button/>')
-        .text('Swap')
-        .on('click', swapBtnClicked);
+    numClicks++; // keeps track of the number of times clicked, hence number of divs made
     $('body')
-        .append('<div></div>')
+        .append($('<div></div>').addClass('red')
         .append(`<p>${numClicks}</p>`)
         .append($('<button/>')
             .text('Swap')
             .on('click', swapBtnClicked))
         .append($('<button/>')
             .text('Delete')
-            .on('click', deleteBtnClicked));
+            .on('click', deleteBtnClicked))
+        );
 }
 
 function swapBtnClicked() {
-    console.log('swap');
+    let el = $(this).parent(); //gets parent div
+    if (el.hasClass('red')) {
+        el.removeClass('red');
+        el.addClass('yellow');
+    } else {
+        el.removeClass('yellow');
+        el.addClass('red');
+    }
 }
 
 function deleteBtnClicked() {
